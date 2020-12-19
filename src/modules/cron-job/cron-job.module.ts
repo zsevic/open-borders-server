17ba3b2d90073fb8a454e7fefd078426c18f1fdd
zsevic <zsevic95@gmail.com>
2020-12-19 21:42:06 +0100

    Run cron job when module is initialized

--- src/modules/cron-job/cron-job.module.ts
+++ src/modules/cron-job/cron-job.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { NlpModule } from 'modules/nlp/nlp.module';
 import { RedisCacheModule } from 'modules/redis-cache/redis-cache.module';
 import { ScraperModule } from 'modules/scraper/scraper.module';
@@ -8,4 +8,8 @@ import { CronJobService } from './cron-job.service';
   imports: [NlpModule, RedisCacheModule, ScraperModule],
   providers: [CronJobService],
 })
-export class CronJobModule {}
+export class CronJobModule implements OnModuleInit {
+  constructor(private readonly cronJobService: CronJobService) {}
+
+  onModuleInit = async () => this.cronJobService.handleCron();
+}

5a4dac5ba6e045cf2adfe711c029dc31b3a3b880
zsevic <zsevic95@gmail.com>
2020-12-31 03:25:52 +0100

    Move formatting country name to the back-end

--- src/modules/country/country.service.ts
+++ src/modules/country/country.service.ts
@@ -10,6 +10,22 @@ export class CountryService {
     const classifiedCountries = await this.redisCacheService.get('countries');
     if (!classifiedCountries) return [];
 
-    return JSON.parse(classifiedCountries);
+    const countryList = JSON.parse(classifiedCountries);
+    return countryList.map(
+      (country: CountryInfo): CountryInfo => ({
+        ...country,
+        name: this.getFormattedCountryName(country.name),
+      }),
+    );
   };
+
+  private getFormattedCountryName = (name: string): string =>
+    name
+      .split(' ')
+      .map(word => {
+        if (word === 'I') return word.toLowerCase();
+        if (word === 'SAD') return word;
+        return word.charAt(0) + word.substring(1).toLowerCase();
+      })
+      .join(' ');
 }
